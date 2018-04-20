7d1f7ff2cc8129c759e46c67636c4db2ed70afc6
Claudio Tezzin Jeremias <claudio.jeremias@clansoft.com.br>
2018-04-20 16:49:56 -0300

    chore: Updating JQuery
    Package json updated.

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -4,7 +4,7 @@ import ReactLoading from 'react-loading';
 
 const LoadingIndicator = () => {
 	return (
-		<div className="debug loading-indicator-bkg">
+		<div className="loading-indicator-bkg">
 			<Row className="mx-auto justify-content-center">
 				<ReactLoading type="bubbles" color="#3cd9b8" height={250} width={100} />
 			</Row>
